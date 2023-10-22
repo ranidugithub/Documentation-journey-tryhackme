9f2d0ed542d7623c37e85733ee06e3551edcbf99
ranidugithub <ranidume@yahoo.com>
2023-10-22 06:03:35 -0400

    adding files

---
+++ Python for Hackers/5_activity1.py
@@ -0,0 +1,10 @@
+#1 Create a greeting your program
+#2 Ask for the user for the name of the pet
+#3 Ask for the name of the city you were born in
+#4 Combine the pet name with the word cyber as a new twitter handle and then add the city they are from.
+#The out put should look like this "Your new twitter handle and bio @cyberfred from Honolulu
+
+print("Hello!! Welcome back to my prgram")
+pet_name = input("Please enter the name of your pet. ")
+city_name = input("Please enter the city you born ")
+print(f'Your new twitter handle and bio @cyber{pet_name} from {city_name}')

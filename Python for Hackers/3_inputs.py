f3aab9962e77f88e6022a337791599c6595da6a6
ranidugithub <ranidume@yahoo.com>
2023-10-22 01:54:17 -0400

    adding files

---
+++ Python for Hackers/3_inputs.py
@@ -0,0 +1,4 @@
+
+# print("The target IP address is " + (IP := input("What is the target IP address? ")))
+ip = input("what is your ip address?? ")
+print(f'your ip is {ip}')
---
+++ Python for Hackers/4_variables.py
@@ -0,0 +1,10 @@
+# x = 5
+# y = "10"
+#
+# print(x + int(y))
+
+#activity
+
+f_name = "Ranidu"
+l_name = "Vishwa"
+full_name = print(f'Your full name is {f_name} {l_name}')
